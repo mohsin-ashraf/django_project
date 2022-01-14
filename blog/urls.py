c0acc6f13565494cac79522e25f4e4224bffd9bf
mohsin <mohsin.ashraf12@gmail.com>
2022-01-14 11:06:42 +0500

    adding password reset module in django project

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,21 @@
 from django.urls import path
+from .views import (
+    PostListView, 
+    DetailPostView, 
+    CreatePostView,
+    UpdatePostView,
+    DeletePostView,
+    PostUserListView
+)
+
 from . import views
 
 urlpatterns = [
-    path('', views.home, name="blog-home"),
+    path('', PostListView.as_view(), name="blog-home"),
+    path('post/<int:pk>/', DetailPostView.as_view(), name='post-detail'),
+    path('post/new/', CreatePostView.as_view(), name='post-create'),
+    path('post/<int:pk>/delete/', DeletePostView.as_view(), name='post-delete'),
+    path('post/<int:pk>/update/', UpdatePostView.as_view(), name='post-update'),
+    path('user/<str:username>', PostUserListView.as_view(), name='user-posts'),
     path('about/', views.about, name="blog-about")
 ]
